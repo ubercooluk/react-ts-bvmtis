a30e0c216e1e49c5970c348b864dc71fd31c1de3
ubercooluk <mailofuk@gmail.com>
2022-08-30 10:19:26 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <p className="yellow">Im yellow</p>
+      <p className="red">Im red</p>
+      <p className="red green">I must be green</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-bvmtis
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-bvmtis)
